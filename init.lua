0408bff6764ebc93aba4ee65f54ee79b2850408a
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-18 16:06:08 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ minetest.register_tool("admintools:Admin Sword", {
     inventory_image = "admintools_admin_sword.png",
     
     tool_capabilities = {
-        full_punch_interval = 0.4,
+        full_punch_interval = 0,
         max_drop_level = 1,
         groupcaps = {
             snappy = {times = {0.01, 0.07, 0.06}, uses = 0, maxlevel = 2},
@@ -19,12 +19,12 @@ minetest.register_tool("admintools:Admin Multitool", {
     inventory_image = "admintools_admin_multitool.png",
     
     tool_capabilities = {
-        full_punch_interval = 0.4,
+        full_punch_interval = 0,
         max_drop_level = 1,
         groupcaps = {
-	        cracky = {times = {[1] = 2.25, [2] = 0.55, [3] = 0.35}, uses = 0, maxlevel = 2},
-	        crumbly = {times = {[1] = 0.70, [2] = 0.35, [3] = 0.20}, uses = 0, maxlevel = 2},
-	        choppy = {times = {[1] = 1.75, [2] = 0.45, [3] = 0.45}, uses = 0, maxlevel = 2},
+	    cracky = {times = {[1] = 2.25, [2] = 0.55, [3] = 0.35}, uses = 0, maxlevel = 2},
+	    crumbly = {times = {[1] = 0.70, [2] = 0.35, [3] = 0.20}, uses = 0, maxlevel = 2},
+	    choppy = {times = {[1] = 1.75, [2] = 0.45, [3] = 0.45}, uses = 0, maxlevel = 2},
             snappy = {times = {0.01, 0.07, 0.06}, uses = 0, maxlevel = 2},
         },
 	groups = {not_in_creative_inventory=1},
