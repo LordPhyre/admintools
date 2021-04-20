4d6bf1b5af752b320f6670ee3bcd56c9699b125f
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-20 12:04:01 -0400

    Delete settings.lua
    no need for this trash

--- settings.lua
+++
@@ -1,9 +0,0 @@
---the base value of how hard the stick hits, though hit force may be up to 30 more than this
-sumo.stick_knockback = 25 
---------------------------------------
---the distance that players may select other players while holding the stick
-sumo.stick_pointing_distance = 7
---------------------------------------
---the cooldown for pushing other players with the stick. If a player re-clicks sooner than this many seconds, they will not have an effect to speak of
-sumo.stick_push_timeout = 0.3 --(float)--in seconds
---------------------------------------
