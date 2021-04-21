2f70b0dbc8e1df03f03c984b079f32f0a1369f6f
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-21 12:37:37 -0400

    Update nodes.lua
    Added Burning Air

--- nodes.lua
+++ nodes.lua
@@ -1,10 +1,3 @@
-minetest.register_craft({
-    type = "shapeless",
-    output = "admintools:barrier",
-    recipe = {"air"}
-})
-
-
 minetest.register_node("admintools:barrier", {
     description = "Barrier Block",
     inventory_image = "admintools_barrier.png",
@@ -20,4 +13,26 @@ minetest.register_node("admintools:barrier", {
     drop = "admintools:barrier",
     sounds = default.node_sound_metal_defaults(),
     groups = {cracky = 1, level = 3},
+})
+
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "admintools:barrier",
+    recipe = {"air"}
+})
+
+
+minetest.register_node("admintools:burningair", {
+    description = "Burning Air",
+    drawtype = "airlike",
+    inventory_image = "admintools_burningair.png",
+    wield_image = "admintools_burningair.png",
+    paramtype = "light",
+    walkable = false,
+    buildable_to = true,
+    sunlight_propagates = true,
+    pointable = false,
+    damage_per_second = 4,
+    drop = "",
 })
