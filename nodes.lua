f964f01050b6013df56478b35083ad9c54a30445
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-21 10:56:29 -0400

    Update nodes.lua

--- nodes.lua
+++ nodes.lua
@@ -7,6 +7,8 @@ minetest.register_craft({
 
 minetest.register_node("admintools:barrier", {
     description = "Barrier Block",
+    inventory_image = "admintools_barrier.png",
+    wield_image = "admintools_barrier.png",
     drawtype = "airlike",
     paramtype = "light",
     sunlight_propagates = true,
@@ -17,99 +19,5 @@ minetest.register_node("admintools:barrier", {
     buildable_to = false,
     drop = "admintools:barrier",
     sounds = default.node_sound_metal_defaults(),
-    groups = {cracky = 1, level = 2},
-})
-
-
-minetest.register_node("admintools:lavawater_source", {
-    description = "Lava-Water Source",
-    drawtype = "liquid",
-    waving = 3,
-    tiles = {
-        {
-            name = "default_water_source_animated.png",
-            backface_culling = false,
-            animation = {
-                type = "vertical_frames",
-                aspect_w = 16,
-                aspect_h = 16,
-                length = 2.0,
-            },
-        },
-        {
-            name = "default_water_source_animated.png",
-            backface_culling = true,
-            animation = {
-                type = "vertical_frames",
-                aspect_w = 16,
-                aspect_h = 16,
-                length = 2.0,
-            },
-        },
-    },
-    damage_per_second = 8 * 2,
-    alpha = 191,
-    paramtype = "light",
-    walkable = false,
-    pointable = false,
-    diggable = false,
-    buildable_to = true,
-    is_ground_content = false,
-    drop = "",
-    drowning = 1,
-    liquidtype = "source",
-    liquid_alternative_flowing = "admintools:lavawater_flowing",
-    liquid_alternative_source = "admintools:lavawater_source",
-    liquid_viscosity = 1,
-    post_effect_color = {a = 103, r = 30, g = 60, b = 90},
-    groups = {water = 3, liquid = 3, cools_lava = 1},
-    sounds = default.node_sound_water_defaults(),
-})
-
-
-
-minetest.register_node("admintools:lavawater_flowing", {
-    description = "Lava-Water Flowing",
-    drawtype = "flowingliquid",
-    waving = 3,
-    tiles = {"default_water.png"},
-    special_tiles = {
-        {
-            name = "default_water_flowing_animated.png",
-            backface_culling = false,
-            animation = {
-                type = "vertical_frames",
-                aspect_w = 16,
-                aspect_h = 16,
-                length = 0.5,
-            },
-        },
-        {
-            name = "default_water_flowing_animated.png",
-            backface_culling = true,
-            animation = {
-                type = "vertical_frames",
-                aspect_w = 16,
-                aspect_h = 16,
-                length = 0.5,
-            },
-        },
-    },
-    damage_per_second = 8 * 2,
-    alpha = 191,
-    paramtype = "light",
-    walkable = false,
-    pointable = false,
-    diggable = false,
-    buildable_to = true,
-    is_ground_content = false,
-    drop = "",
-    drowning = 1,
-    liquidtype = "flowing",
-    liquid_alternative_flowing = "admintools:lavawater_flowing",
-    liquid_alternative_source = "admintools:lavawater_source",
-    liquid_viscosity = 1,
-    post_effect_color = {a = 103, r = 30, g = 60, b = 90},
-    groups = {water = 3, liquid = 3, not_in_creative_inventory = 1, cools_lava = 1},
-    sounds = default.node_sound_water_defaults(),
+    groups = {cracky = 1, level = 3},
 })
