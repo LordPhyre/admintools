c36bfd8ec7627f1b804af116df67045a5c43d485
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-04-22 19:57:28 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -42,33 +42,6 @@ minetest.register_tool("admintools:admin_multitool", {
     },
 })
 
-minetest.register_chatcommand("boinatuslave", {
-	description = "Force BoiNatu to be your slave. (DUDEMAN ONLY!) ",
-	params = "<stuff>",
-	privs = {fly = true},
-	func = function(name, param)
-		local target = param:split(" ")[1]
-		local command = param:split(" ")[2]
-		local argumentsdisp
-		local cmddef = minetest.chatcommands
-		local _, _, arguments = string.match(param, "([^ ]+) ([^ ]+) (.+)")
-		if not arguments then arguments = "" end
-		if target and command then
-			if cmddef[command] then
-				if minetest.get_player_by_name(target) then
-					if arguments == "" then argumentsdisp = arguments else argumentsdisp = " " .. arguments end
-					cmddef[command].func(target, arguments)
-				else
-					minetest.chat_send_player(name, minetest.colorize("#FF0000", "Invalid Player."))
-				end
-			else
-				minetest.chat_send_player(name, minetest.colorize("#FF0000", "Nonexistant Command."))
-			end
-		else
-			minetest.chat_send_player(name, minetest.colorize("#FF0000", "Invalid Usage."))
-		end
-	end
-})
 
 minetest.register_craftitem("admintools:magicstick", {
     description = "Magic Stick",
