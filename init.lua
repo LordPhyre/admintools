4bd2d8b9a77125478242e071f8ac5343339a2aab
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-06-30 11:09:41 -0400

    Update init.lua
    Added dofile for spectator_mode

--- init.lua
+++ init.lua
@@ -118,3 +118,4 @@ minetest.register_craftitem("admintools:magicstick", {
 
 
 dofile(minetest.get_modpath("admintools") .. "/nodes.lua")
+dofile(minetest.get_modpath("admintools") .. "/spectator.lua")
