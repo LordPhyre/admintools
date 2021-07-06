3b2d40d6f99ca1f3ea5de8d151b31e42324fa2d8
LordPhyre <60118488+LordPhyre@users.noreply.github.com>
2021-07-06 15:49:20 -0400

    -obamium code
    Making a seperate mod!

--- nodes.lua
+++ nodes.lua
@@ -37,29 +37,3 @@ minetest.register_node("admintools:burningair", {
     damage_per_second = 4,
     drop = "",
 })
-
-minetest.register_node("admintools:obamium", { --the obamium bloc
-    description = "Obamium",
-    drawtype = "mesh",
-    mesh = "obamium.obj",
-    sunlight_propagates = true,
-    tiles = {
-		"obamiumtext.png"
-	},
-    sounds = default.node_sound_glass_defaults(),
-    groups = {fleshy = 1, level = 3},
-})
-
-minetest.register_node("admintools:obamium_ore", { --the obamium ore
-    description = "Raw Obamium",
-    tiles = {"default_stone.png^obamium_ore.png"},
-    sounds = default.node_sound_stone_defaults(),
-    groups = {cracky = 1, level = 3, not_in_creative_inventory = 1},
-    drop = "admintools:obamium"
-})
-
-minetest.register_craft({
-	type = "fuel",
-	recipe = "admintools:obamium",
-	burntime = 69420123,
-})
